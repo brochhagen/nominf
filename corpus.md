5739b4c7e8b9e987171bd4c0ab7b3f2904c326b5
brochhagen <brox@broxs-Air.home>
2019-03-22 15:06:34 +0100

    sketch parsing for plain text

--- corpus.md
+++ corpus.md
@@ -11,8 +11,15 @@ Following the lead of Grimm & McNally (2015), we want to take a quantitative loo
 
 
 ### Cleaning
-
 * Exclude false infinitives **is there a syntactic criterion we could use for filtering? Otherwise use RAE or similar as criterion for lexicalization**
+
+
+#### Plain text
+
+* I would not remove the special symbol: "@". As far as I can see, it:
+	* Indicates a new fragment in the corpus when its doubled and followed by a digit, e.g. *@@1100*
+	* Indicates that trailing text is missing, e.g. *and then the doctor said @ @ @ @ @ @ @ @ @ Jane was content.* Removing these markers could cause confusion by assuming that the text continues where it doesn't.
+
 
 ### Categorization
 
--- text-extract.py
+++ text-extract.py
@@ -1,12 +1,30 @@
 import os
 import re
- 
-directory = os.fsencode('CdE/text_AR-tez/') # directory with individual text files
-pattern = r'(?:[^ ]+ ){0,3}\w+er|ir|ar\b(?: [^ ]+){0,3}'#pattern we're after as a regular expression, see https://regex101.com/r/pB3eW0/1
 
-for file in os.listdir(directory):
+window_size = 3
 
-	filename = os.fsdecode(file)
-	with open(os.fsdecode(directory)+filename, 'r') as f:
-		result = re.findall(pattern, f.read())
-		print(result)
+#directory = os.fsencode('CdE/text_AR-tez/') # directory with individual text files
+with open('CdE/text_AR-tez/AR-B-00.txt') as f:
+	with open('CdE/text_output/text_data.txt', mode = 'w') as h:
+		for line in f:
+			words = line.split()
+			for i in range(len(words)):
+				if words[i][-2:] in ['ar', 'er', 'ir']:
+					if i > 0:
+						if words[i][0].islower() or words[i-1][-1] == '.':
+							if i < window_size: 
+								left_window = i-1
+							else:
+								left_window = i-window_size
+							if len(words) < i+window_size:
+								right_window = len(words)
+							else:
+								right_window = i+window_size+1
+							h.write('%s\n' % ' '.join(words[left_window:right_window]))
+
+# for file in os.listdir(directory):
+
+# 	filename = os.fsdecode(file)
+# 	with open(os.fsdecode(directory)+filename, 'r') as f:
+# 		result = re.findall(pattern, f.read())
+# 		print(result)
